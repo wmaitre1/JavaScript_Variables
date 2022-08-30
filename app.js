14ad8a8acffce6c0f8dfa3f7ceac58d8d9767da6
William Maitre <william.maitre@gmail.com>
2022-08-30 16:54:46 -0400

    finished exercise

--- app.js
+++ app.js
@@ -5,6 +5,25 @@ console.log(
 
 // Exercise 1
 
+  const firstName = 'William'
+
+  let lastName = 'Maitre'
+
+  var age = 41;
+
 // Exercise 2
 
+  let fullName = '${firstName} ${lastName}';
+
+  console.log(fullName);
+
+
 // Exercise 3
+
+  let city = 'Land O Lakes';
+
+  let pasttime ='reading';
+
+  let myStory = `\Hello! My name is ${fullName}. I live in ${city}. I enjoy ${passtime} and coding!`;
+
+  console.log(myStory);
